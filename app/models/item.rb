be1d4166544ba665e450b8f033354e6b343ee34c
sasami-street-bb <myui076v@gmail.com>
2020-09-09 14:24:39 +0900

    ActiveHashのアソシエーションとバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_fee
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :shipping_date
+
+  validates :name, :description, :price, :category, :condition, :shipping_fee, :prefecture, :shipping_date, presence: true
+  validates :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_date_id, numericality: { other_than: 0 } 
 end
