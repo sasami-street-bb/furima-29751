9b5ec91b0e07ab3f75e23e9e332b80ba34550a2c
sasami-street-bb <myui076v@gmail.com>
2020-09-09 15:37:27 +0900

    itemモデルアソシエーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,8 @@ class Item < ApplicationRecord
   belongs_to_active_hash :shipping_fee
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :shipping_date
+  belongs_to :user, null: false, foreign_key: true
+  has_one_attached :image
 
   validates :name, :description, :price, :category, :condition, :shipping_fee, :prefecture, :shipping_date, presence: true
   validates :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_date_id, numericality: { other_than: 0 } 
