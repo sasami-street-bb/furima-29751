da70447687c0eae12b7c81f50a71f1994e296624
sasami-street-bb <myui076v@gmail.com>
2020-09-10 11:19:27 +0900

    itemモデルテストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,7 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :name, :description, :price, :category, :condition, :shipping_fee, :prefecture, :shipping_date, presence: true
+  validates :name, :description, :price, :category, :condition, :shipping_fee, :prefecture, :shipping_date, :image, presence: true
   validates :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_date_id, numericality: { other_than: 0 } 
 
   validates  :price, numericality: { only_integer: true, greater_than: 300, less_than: 10000000 }
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name {Faker::Name.name}
+    description {Faker::Lorem.sentence}
+    price {Faker::Number.number(digits: 6)}
+    category_id {Faker::Number.within(range: 1..2)}
+    condition_id {Faker::Number.within(range: 1..2)}
+    shipping_fee_id	{Faker::Number.within(range: 1..2)}
+    prefecture_id {Faker::Number.within(range: 1..2)}
+    shipping_date_id {Faker::Number.within(range: 1..2)}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('app/assets/images/camera.png'), filename: 'test_image.png')
+    end
   end
 end
