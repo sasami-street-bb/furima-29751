b0c9a332bd17be3298708b293daf223295f84169
sasami-street-bb <myui076v@gmail.com>
2020-09-12 23:34:14 +0900

    orderモデルのアソシエーション、コントローラー記述

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,38 @@
+class OrdersController < ApplicationController
+  before_action :set_item, only: :index
+
+  def index
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+  
+  private
+
+  def order_params
+    params.permit(:token)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: order_params[:price],
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+end
