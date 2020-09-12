3f2b40b614cdb489d2979a0d741799d6377ab4d2
sasami-street-bb <myui076v@gmail.com>
2020-09-12 23:36:44 +0900

    ルーティング、リンク先修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :items
-  resources :orders, only: :index
+  resources :items do
+    resources :orders, only: :index
+  end
+  
 end
