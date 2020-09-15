b82a2bbc2a1848cfea7df9593756dbe250a1a846
sasami-street-bb <myui076v@gmail.com>
2020-09-15 12:03:39 +0900

    クレジット決済機能のためのコントローラーとJS記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,20 @@
 class OrdersController < ApplicationController
-  before_action :set_item, only: :index
+  before_action :set_item
 
   def index
-    @order = Order.new
+    @order = OrderItem.new
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
+
+  def new
+    @order = OrderItem.new
   end
 
   def create
-    @order = Order.new(order_params)
+    @order = OrderItem.new(order_params)
+    binding.pry
     if @order.valid?
       pay_item
       @order.save
@@ -19,14 +27,14 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.permit(:token)
+    params.require(:order_item).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :token).merge(user_id: current_user.id, item_id: params[:item_id],token: params[:token])
   end
 
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
-      amount: order_params[:price],
-      card: order_params[:token],
+      amount: @item.price,
+      card: params[:token],
       currency: 'jpy'
     )
   end
